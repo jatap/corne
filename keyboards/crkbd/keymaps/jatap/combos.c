6be42ec0a00bd795227afeb017a043541b688fc9
Julio Antúnez Tarín <julio.antunez.tarin@gmail.com>
2023-02-25 08:56:18 +0000

    refactor: disable tmux combo

--- keyboards/crkbd/keymaps/jatap/combos.c
+++ keyboards/crkbd/keymaps/jatap/combos.c
@@ -11,10 +11,10 @@ enum combo_events {
 uint16_t COMBO_LEN = COMBO_LENGTH; 
 
 const uint16_t PROGMEM clear_combo[] = {KC_D, KC_C, COMBO_END};
-const uint16_t PROGMEM tmux_combo[] = {KC_H, KC_COMM, COMBO_END};
+// const uint16_t PROGMEM tmux_combo[] = {KC_H, KC_COMM, COMBO_END};
 
 combo_t key_combos[] = {
-    [TMUX] = COMBO_ACTION(tmux_combo),
+    // [TMUX] = COMBO_ACTION(tmux_combo),
     [CLEAR_SCREEN] = COMBO_ACTION(clear_combo),
 };
 
@@ -25,10 +25,10 @@ void process_combo_event(uint16_t combo_index, bool pressed) {
         SEND_STRING("cls\n");
       }
       break;
-    case TMUX:
-      if (pressed) {
-        tap_code16(LCTL(KC_SPC));
-      }
-      break;
+    // case TMUX:
+    //   if (pressed) {
+    //     tap_code16(LCTL(KC_SPC));
+    //   }
+    //   break;
   }
 }
