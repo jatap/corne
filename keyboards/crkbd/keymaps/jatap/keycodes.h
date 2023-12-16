4e3e61871f41653f2b533cf28fa4c478cf8b9666
Julio Antúnez Tarín <julio.antunez.tarin@gmail.com>
2023-12-16 23:28:10 +0100

    refactor: promote GUI before ALT (homemods - emacs)

--- keyboards/crkbd/keymaps/jatap/keycodes.h
+++ keyboards/crkbd/keymaps/jatap/keycodes.h
@@ -3,16 +3,16 @@
 #include QMK_KEYBOARD_H
 
 // Left-hand home row mods
-#define HOME_A LGUI_T(KC_A)
-#define HOME_R LALT_T(KC_R)
+#define HOME_A LALT_T(KC_A)
+#define HOME_R LGUI_T(KC_R)
 #define HOME_S LCTL_T(KC_S)
 #define HOME_T LSFT_T(KC_T)
 
 // Right-hand home row mods
 #define HOME_N RSFT_T(KC_N)
 #define HOME_E RCTL_T(KC_E)
-#define HOME_I LALT_T(KC_I)
-#define HOME_O RGUI_T(KC_O)
+#define HOME_I LGUI_T(KC_I)
+#define HOME_O LALT_T(KC_O)
 
 // Layers
 #define LAYER_MEDIA LT(1, KC_TAB)
