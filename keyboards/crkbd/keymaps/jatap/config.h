8751a32bdfd70b05d19e982cac7c92a1fb30680e
Julio Antúnez Tarín (Software Engineer Manager) <julio.antunez.tarin@gmail.com>
2025-05-16 19:54:22 +0200

    fix: chordal_hold

--- keyboards/crkbd/keymaps/jatap/config.h
+++ keyboards/crkbd/keymaps/jatap/config.h
@@ -31,7 +31,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define TAPPING_TERM_PER_KEY
 #define QUICK_TAP_TERM 120
 #define TAPPING_FORCE_HOLD_PER_KEY
-#define CHORDAL_HOLD_PER_KEY
+#define CHORDAL_HOLD
+/* #define CHORDAL_HOLD_PER_KEY */
 /* #define HOLD_ON_OTHER_KEY_PRESS_PER_KEY */
 #define PERMISSIVE_HOLD_PER_KEY
 #define TAPPING_TOGGLE 5
